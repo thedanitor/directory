edc082bd076aff5bfef26c6d69b6a1bf49a72ff5
Dan Weikart <danmweikart@gmail.com>
2020-07-14 14:30:50 -0700

    Added Jumbotron component

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Jumbotron(props) {
+    return (
+        <div class="jumbotron">
+  <h1 class="display-4">Employee Directory!</h1>
+  <p class="lead">Click on carrots to filter by heading or use the search box below to narrow your results.</p>
+</div>
+    );
+}
+
+export default Jumbotron;
