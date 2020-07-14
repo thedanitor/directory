9034b02144e75b5ab8e079809623fe368c86a4a5
Dan Weikart <danmweikart@gmail.com>
2020-07-14 14:51:38 -0700

    Added TableHeader component.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Wrapper from "./components/Wrapper";
 import Jumbotron from "./components/Jumbotron";
 import SearchBar from "./components/SearchBar";
+import TableHeader from "./components/TableHeader";
 
 function App() {
   return (
     <Wrapper>
       <Jumbotron />
       <SearchBar />
+      <TableHeader />
     </Wrapper>
   );
 }
---
+++ src/components/TableHeader/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function TableHeader() {
+    return (
+        <div className="container">
+      <div className="row">
+        <div className="col-2">Image</div>
+        <div className="col-2">Name</div>
+        <div className="col-2">Phone</div>
+        <div className="col-2">Email</div>
+        <div className="col-2">DOB</div>
+
+      </div>
+    </div>
+    );
+}
+
+export default TableHeader;
